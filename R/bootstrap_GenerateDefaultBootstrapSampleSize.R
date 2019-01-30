882659ae1174eefa7d2a07770327b1188c467722
stork119 <karol.nienaltowski@gmail.com>
2019-01-30 10:14:42 +0100

    Default Bootstrap Sample Size set to 1000

---
+++ R/bootstrap_GenerateDefaultBootstrapSampleSize.R
@@ -0,0 +1,14 @@
+#' GenerateDefaultBootstrapSampleSize
+#'
+#' @param model ITRCModel
+#' @param bootstrap.number
+#' @param bootstrap.sample_size
+GenerateDefaultBootstrapSampleSize <-
+  function(
+  model,
+  bootstrap.sample_size,
+  ...
+  ) {
+    return(1000)
+  }
+
