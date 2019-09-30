85bee9c1016f5ec97bd1aaa4e1587e40b4cad69a
stork119 <karol.nienaltowski@gmail.com>
2019-09-30 13:16:14 +0200

    Plotting confusion matrix

---
+++ R/graphics_plotConfusionMatrix.R
@@ -0,0 +1,95 @@
+#' plotITRCWaves
+#'
+#' @description This functions return ggplot2 figure that visualise Information
+#'  Theoretic Response Curves and specificity of cellular response to particular signals.
+#'
+#' @param model ITRCModel object return by ITRC function
+#' @param confusion.signal signal for which you want to plot confusion matrix, default \code{confusion.signal == max(signal)}
+#' @param title_ character, specify title of plot, default \code{"Information Theoretic Response Curve"}
+#' @param xlab_ character, label of x axes, default \code{"States number"}
+#' @param ylab_ character, label of y axes and legend title, default \code{"Signal levels"}
+#' @details TODO important
+#' @export
+plotCofusionMatrix <-
+  function(
+    model,
+    confusion.signal = NULL,
+    save.plot = FALSE,
+    title_ = "",
+    ylab_ ="Specific Stimulation Level",
+    xlab_ = "Stimulation Level",
+    ...
+  ){
+
+    if(is.null(model)){
+      stop("model must be an object of class ITRCModel")
+    } else if(class(model) != "ITRCModel"){
+      stop("model must be an object of class ITRCModel")
+    }
+
+    if(is.null(model$confusion.table)){
+      stop("model must be an object of class ITRCModel and should contain confusion.table")
+    }
+
+    x_ = "1"
+    y_ = "prob"
+    fill_ = "fill"
+
+    # # ggplot2::model$confusion.table
+    # # df.confusion.fill <- df.confusion$fill
+    # # names(df.confusion.fill) <- df.confusion$fill
+    #
+    # summary_vars <-syms(experiments.df$response.cols)
+    #
+    # # Wrap the summary variables with mean()
+    # summary_vars <- purrr::map(summary_vars, function(var) {
+    #   expr(fun.summarise(!!var, na.rm = TRUE))
+    # })
+    #
+    # # Prefix the names with `avg_`
+    # names(summary_vars) <- experiments.df$response.protein
+    confusion.signal.syms <- quo(max.signal == max(!!sym(model$signal)))
+    if(!is.null(confusion.signal)){
+      if(!is.numeric(confusion.signal)){
+        confusion.signal.syms <- quo(max.signal == confusion.signal)
+      }
+    }
+
+
+
+    df.confusion <-
+      model$confusion.table %>%
+      dplyr::filter(!!confusion.signal.syms) %>%
+      dplyr::mutate(
+        fill =
+          dplyr::if_else(!!quo(!!sym(model$signal) == !!sym(model$class)),
+                         ggthemes::canva_palettes[["Subdued and proffesional"]][2],
+                         ggthemes::canva_palettes[["Subdued and proffesional"]][1]))
+
+    fill.values <- df.confusion$fill
+    names(fill.values) <- fill.values
+
+    ggplot2::ggplot(
+        df.confusion,
+        mapping =
+          ggplot2::aes_string(
+            x = x_,
+            y = y_,
+            fill = fill_
+          )
+      ) +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::facet_grid("Stim~class",
+                          switch = "both"
+      ) +
+      ggplot2::coord_polar(theta = "y", start = 0) +
+      ggplot2::ylim(c(0,1)) +
+      theme_itrc.confusion_matrix(theme.title_size = 12) +
+      ggplot2::ylab(ylab_) +
+      ggplot2::xlab(xlab_) +
+      ggplot2::ggtitle(title_) +
+      ggplot2::scale_fill_manual(values = fill.values,
+                                 guide = "none"
+                                 ) %>%
+    return()
+  }
---
+++ R/graphics_themes_ThemeConfusionMatrix.R
@@ -0,0 +1,67 @@
+#' theme_itrc.confusion_matrix
+#'
+#' @description This function specify theme used in the ITRC package
+#'
+#' @param  theme.base_size = 12,
+#' @param  theme.base_family = "sans",
+#' @param  theme.title_size = 36,
+#' @param  theme.text_size = 3*theme.title_size/4,
+#' @param  theme.margins = c(1,1, 1, 1),
+#' @param  legend.position = "right",
+#'
+#' @return ggthemes::theme_foundation object
+#'
+#' @export
+theme_itrc.confusion_matrix <-
+  function (theme.base_size = 12,
+                                         theme.base_family = "sans",
+                                         theme.title_size = 36,
+                                         theme.text_size = 3*theme.title_size/4,
+                                         theme.margins = c(1,1, 1, 1),
+                                         legend.position="right",
+                                         ...)
+{
+  (ggthemes::theme_foundation(
+    base_size = theme.base_size,
+    base_family = theme.base_family) +
+     ggplot2::theme(line = ggplot2::element_line(),
+                    rect = ggplot2::element_rect(fill =
+                                                   ggthemes::ggthemes_data$fivethirtyeight$value[3],
+                                                 linetype = 0, colour = NA),
+                    plot.title = ggplot2::element_text(colour =
+                                                         ggthemes::ggthemes_data$fivethirtyeight$value[1],
+                                                       vjust = 1, hjust = 0.5,
+                                                       size=theme.title_size,
+                                                       face="bold"),
+                    text = ggplot2::element_text(colour = ggthemes::ggthemes_data$fivethirtyeight$value[1]),
+                    #axis.title = ggplot2::element_blank(),
+                    axis.title = ggplot2::element_text(size= 16,
+                                                       face="bold",
+                                                       vjust = 0.5),
+                    #axis.title.y = ggplot2::element_text(angle=90),
+                    #axis.text.y = ggplot2::element_blank(),
+                    axis.text = ggplot2::element_blank(),#ggplot2::element_text(size=theme.text_size, face="bold"),
+                    #axis.text.x = ggplot2::element_text(angle = 90),
+                    axis.ticks = ggplot2::element_blank(),
+                    axis.line.x = ggplot2::element_blank(),
+                    axis.line.y = ggplot2::element_blank(),
+                    legend.position=legend.position,
+                    legend.background = ggplot2::element_rect(fill = "white"),
+
+                    panel.grid = ggplot2::element_line(colour = NULL),
+                    panel.grid.major =
+                      ggplot2::element_line(colour = ggthemes::ggthemes_data$fivethirtyeight$value[2]),
+                    panel.grid.minor =
+                      ggplot2::element_blank(),
+                    panel.background = ggplot2::element_rect(fill = "white"),
+                    # plot.title = ggplot2::element_text(hjust = 0, size = rel(1.75), face = "bold"),
+                    plot.margin = ggplot2::unit(theme.margins, "lines"),
+                    plot.background = ggplot2::element_rect(fill = "white"),
+                    strip.background = ggplot2::element_rect(fill = "white"),
+                    #strip.text.y = ggplot2::element_text(angle = 180),
+                    strip.text =
+                      ggplot2::element_text(size= 12,
+                                            face="bold",
+                                            vjust = 0.5#,lineheight = theme.text_size*3
+                      )))
+}
